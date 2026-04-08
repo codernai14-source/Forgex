f7b110b9c2a5df013679d9640427fdb4cae81be5
yuan <huang_qijuan@163.com>
2026-04-08 21:29:27 +0800

    feat(java): 添加Java

---
+++ linshiproduct/day01-helloworld-app/src/com/itheima/comment/Comment.java
@@ -0,0 +1,16 @@
+package com.itheima.comment;
+
+public class Comment {
+    static void main(String[] args) {
+        //打印两行诗句
+        System.out.println("HelloWorld!");
+        /*
+        多行注释
+        打印三行诗句
+         */
+        System.out.println("HelloWorld!");
+        System.out.println("床前明月光，疑是地上霜");
+        System.out.println("HelloWorld!");
+
+    }
+}
---
+++ linshiproduct/day01-helloworld-app/src/com/itheima/speedkey/speedkey.java
@@ -0,0 +1,9 @@
+package com.itheima.speedkey;
+
+public class speedkey {
+    static void main(String[] args) {
+        System.out.println("xixi");
+        System.out.println("xixi");
+        System.out.println("xixi");
+    }
+}
